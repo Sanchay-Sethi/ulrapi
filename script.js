32130a1e1d7ead04ff9f5ba6dd27de58584408f5
Sanchay Sethi <56469895+Sanchay-Sethi@users.noreply.github.com>
2020-10-19 14:57:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,100 @@
+window.onscroll = function() {myFunction()};
+		
+function myFunction() {
+	var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+	var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+	var scrolled = (winScroll / height) * 100;
+	document.getElementById("myBar").style.width = scrolled + "%"; 
+}
+
+var i = 2;
+function switchTAB() {
+	var x = document.getElementById("list-switch");
+	if(i%2 == 0) {
+		document.getElementById("list-switch").style= "display: grid; height: 50vh; margin-left: 5%;";
+		document.getElementById("search-switch").style= "display: block; margin-left: 5%;"; i++;
+	}else {
+		document.getElementById("list-switch").style= "display: none;";
+		document.getElementById("search-switch").style= "display: none;"; i++;
+	}
+}
+
+
+// For LOGIN
+var x = document.getElementById("login");
+var y = document.getElementById("register");
+var z = document.getElementById("btn");
+var a = document.getElementById("log");
+var b = document.getElementById("reg");
+var w = document.getElementById("other");
+
+function register() {
+  x.style.left = "-400px";
+  y.style.left = "50px";
+  z.style.left = "110px";
+  w.style.visibility = "hidden";
+  b.style.color = "#fff";
+  a.style.color = "#000";
+}
+
+function login() {
+  x.style.left = "50px";
+  y.style.left = "450px";
+  z.style.left = "0px";
+  w.style.visibility = "visible";
+  a.style.color = "#fff";
+  b.style.color = "#000";
+}
+    
+function goFurther(){
+  if (document.getElementById("chkAgree").checked == true) {
+    document.getElementById('btnSubmit').style = 'background: linear-gradient(to right, #a654ea, #00d4ff);';
+  }
+  else{
+    document.getElementById('btnSubmit').style = 'background: lightgray;';
+  }
+}
+
+function google() {
+  	window.location.assign("https://accounts.google.com/signin/v2/identifier?service=accountsettings&continue=https%3A%2F%2Fmyaccount.google.com%2F%3Futm_source%3Dsign_in_no_continue&csig=AF-SEnbZHbi77CbAiuHE%3A1585466693&flowName=GlifWebSignIn&flowEntry=AddSession", "_blank");
+}
+
+// QUIZ
+
+function quizt(frame) {
+  // document.getElementById('quiz-container').style='display:block;';
+  document.getElementById('f1').style='display: none;';
+  document.getElementById('f2').style='display: none;';
+  document.getElementById('f3').style='display: none;';
+  document.getElementById('f4').style='display: none;';
+  document.getElementById('f5').style='display: none;';
+  document.getElementById('f6').style='display: none;';
+  document.getElementById('f7').style='display: none;';
+  document.getElementById('f8').style='display: none;';
+  document.getElementById('f9').style='display: none;';
+  document.getElementById('f10').style='display: none;';
+  document.getElementById('f11').style='display: none;';
+  if(frame == 1) document.getElementById('f1').style = 'display: block';
+  else if(frame == 2) document.getElementById('f2').style = 'display: block';
+  else if(frame == 3) document.getElementById('f3').style = 'display: block';
+  else if(frame == 4) document.getElementById('f4').style = 'display: block';
+  else if(frame == 5) document.getElementById('f5').style = 'display: block';
+  else if(frame == 6) document.getElementById('f6').style = 'display: block';
+  else if(frame == 7) document.getElementById('f7').style = 'display: block';
+  else if(frame == 8) document.getElementById('f8').style = 'display: block';
+  else if(frame == 9) document.getElementById('f9').style = 'display: block';
+  else if(frame == 10) document.getElementById('f10').style = 'display: block';
+  else if(frame == 11) document.getElementById('f11').style = 'display: block'; 
+  else alert('error');
+}
+
+function startquiz() {
+  document.getElementById('title').style = 'display: none;'; 
+
+  document.getElementById('panel').style = 'display: inline-flex;'; 
+  document.getElementById('left').style = 'display: block;'; 
+  document.getElementById('right').style = 'display: block;'; 
+}
+function searchdisplay() {
+  document.getElementById('searchpanel').style.display="block";
+}
